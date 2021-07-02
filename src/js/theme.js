ba88788e93a1ff26ad693da3320a2df5cc84d3a9
Anastasia Gelyuta <gelyuta@gmail.com>
2021-07-02 16:54:48 +0300

    Добавила функционал изменения темы при нажатии на чекбокс

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,8 +1,8 @@
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 const inputEl = document.querySelector('#theme-switch-toggle')
 inputEl.addEventListener('change', onThemeSwich)
 console.log(inputEl)
@@ -10,14 +10,15 @@ console.log(inputEl)
 const body =document.querySelector('body')
 console.log(body)
 
-  function onThemeSwich(){
-  
- if (inputEl.checked === true){
-  document.body.classList.add(Theme.DARK) 
+function onThemeSwich(){
+
+if (inputEl.checked === true){
+document.body.classList.add(Theme.DARK) 
 } else {
-  document.body.classList.remove(Theme.DARK)
-  document.body.classList.add(Theme.LIGHT) 
-}
- } 
+document.body.classList.remove(Theme.DARK)
+document.body.classList.add(Theme.LIGHT) 
+  }
+} 
+
 
  
