f23da955cb06c468ed63bb14239a9d3b899fcc6f
Anastasia Gelyuta <gelyuta@gmail.com>
2021-07-04 16:02:46 +0300

    При перезагрузке страницы сохраняется текущая тема

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.css';
 import './js/menu';
 import './js/theme';
+
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,24 +1,57 @@
-
+// Темы
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+// Получаю доступ к переключателю
 const inputEl = document.querySelector('#theme-switch-toggle')
+
+// Вешаю слушатель событий на инпут
 inputEl.addEventListener('change', onThemeSwich)
-console.log(inputEl)
+// console.log(inputEl)
 
 const body =document.querySelector('body')
 console.log(body)
 
+// Переключение темы со светлой на темную и наоборот
 function onThemeSwich(){
 
 if (inputEl.checked === true){
-  document.body.classList.remove(Theme.LIGHT)
-document.body.classList.add(Theme.DARK) 
+  document.body.classList.remove(Theme.LIGHT);
+  document.body.classList.add(Theme.DARK);
+  inputEl.checked = true;
+
+  localStorage.setItem('current-theme', Theme.DARK);
 } else {
-  document.body.classList.remove(Theme.DARK)
-document.body.classList.add(Theme.LIGHT) 
+  document.body.classList.remove(Theme.DARK);
+  document.body.classList.add(Theme.LIGHT);
+
+  localStorage.setItem('current-theme', Theme.LIGHT);
+  inputEl.checked.false;
   }
 } 
 
- 
+// Текущая тема
+function onCurrentTheme() {
+  const currentTheme = localStorage.getItem('current-theme');
+  if(currentTheme === Theme.LIGHT){
+    document.body.classList.remove(Theme.DARK);
+    document.body.classList.add(Theme.LIGHT);
+  
+    localStorage.setItem('current-theme', Theme.LIGHT);
+    inputEl.checked.false;
+  } else {
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
+    inputEl.checked = true;
+  
+    localStorage.setItem('current-theme', Theme.DARK);
+  }
+}
+
+onCurrentTheme()
+  
+     
+
+   
